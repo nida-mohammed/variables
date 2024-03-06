896bef585d13962b64116598f264348f53e2be76
nida-mohammed <162143055+nida-mohammed@users.noreply.github.com>
2024-03-06 18:43:44 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+//// variables
+//data type known as core type also: string,number,boolean
+//type :let,var,const (var is the old version and let is latest)
+//we dont use var but use let as it shows error in the start
+var myname = "nida mohammed";
+console.log(myname);
+var num = 1234;
+console.log(num);
+var boolean = "true";
+console.log(true);
+//strong typing
+var myname1 = "nida mohammed";
+console.log(myname1);
+var age = 35;
+console.log(age);
+var itraininlondon = true;
+console.log(itraininlondon);
---
+++ main.ts
@@ -0,0 +1,24 @@
+//// variables
+//data type known as core type also: string,number,boolean
+//type :let,var,const (var is the old version and let is latest)
+//we dont use var but use let as it shows error in the start
+
+let myname ="nida mohammed";
+console.log(myname);
+
+let num =1234;
+console.log(num);
+
+let boolean ="true";
+console.log(true);
+
+//strong typing
+
+let myname1:string = "nida mohammed";
+console.log(myname1);
+
+let age:number =35;
+console.log(age);
+
+let itraininlondon:boolean = true;
+console.log(itraininlondon);
